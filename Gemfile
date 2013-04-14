99f0d00609cb1faeac1905fe646f665caa043ddd
Caius Durling <dev@caius.name>
2013-04-15 00:39:46 +0100

    Remove newrelic

--- Gemfile
+++ Gemfile
@@ -4,7 +4,6 @@ gem "sinatra", :require => "sinatra/base"
 gem "actionpack", :require => "action_view"
 gem "activesupport", require: "active_support/core_ext/hash/keys"
 gem "twitter"
-gem "newrelic_rpm"
 gem "hashie"
 gem "rake"
 
--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ default_run_options[:pty] = true
 
 set :use_sudo, false
 
-set :local_shared_files, %w(config/newrelic.yml config/twitter.yml)
+set :local_shared_files, %w(config/twitter.yml)
 set :local_shared_dirs, %w(cache)
 
 set :global_shared_files, []
