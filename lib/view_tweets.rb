0d365b9f20cfcf6e0fdaabeb2a48df311262e0ea
Caius Durling <dev@caius.name>
2013-04-15 00:25:25 +0100

    Setup twitter config on app boot

--- lib/view_tweets.rb
+++ lib/view_tweets.rb
@@ -14,7 +14,18 @@ class ViewTweets
   def self.lib
     root + "lib"
   end
+  def self.setup_twitter config
+    Twitter.configure do |c|
+      c.consumer_key = config["consumer_key"]
+      c.consumer_secret = config["consumer_secret"]
+      c.oauth_token = config["token"]
+      c.oauth_token_secret = config["secret"]
+    end
+  end
 end
+
+# Setup twitter auth
+ViewTweets.setup_twitter(YAML.load(ViewTweets.root.join("config/twitter.yml").read))
 
 %w(core_ext/array_to_sentence app grab_reply_chain grabber).each do |file|
   require "#{ViewTweets.lib}/view_tweets/#{file}"
